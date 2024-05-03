de110f30b04b4c5c1883f418a1c99d41fe45930a
michel22miranda <165222330+michel22miranda@users.noreply.github.com>
2024-05-03 20:25:23 -0300

    Add files via upload

--- estoque.py
+++ estoque.py
@@ -1,19 +1,53 @@
 from fastapi import FastAPI
+import sqlite3
 
 app = FastAPI()
 
-@app.get("/debito/{produto}/{quantidade}")
-def debito(produto:str, quantidade:int):
-    return{"msg":"debito executado com sucesso"}
+@app.get("/extrato/{dtinicial}/{dtfinal}")
+def extrato(dtinicial:int, dtfinal:int):
+    return {"msg":"ok"}
 
-@app.get("/credito/{produto}/{quantidade}")
-def credito(produto:str, quantidade:int):
-    return{"msg":"credito executado com sucesso"}
+@app.put("/credito/{codigo}/{quantidade}")
+def credito(codigo:int, quantidade:int):
+    conn = sqlite3.connect("/home/ijovem03/turma202402/database/estoque.db")
+    cur = conn.cursor()
 
-@app.get("/extrato_diario/{data}")
-def extrato_diario(data:int):
-    return{"msg":"ok"}
+    cur.execute(""" INSERT INTO registro (id_produto, qtde, operacao) VALUES (?,?, 'CREDITO') """, ( codigo, quantidade,))
+    conn.commit()
+    conn.close()
 
-@app.get("/extrato_mensal/{ano}/{mes}/{dia}")
-def extrato_mensal(ano:int,mes:int,dia:int):
-    return{"msg":"ok"}
+    atualizar_credito(codigo, quantidade)
+    
+    return {"msg":"credito efetuado"}
+
+@app.put("/debito/{codigo}/{quantidade}")
+def debito(codigo:int, quantidade:int):
+    conn = sqlite3.connect("/home/ijovem03/turma202402/database/estoque.db")
+    cur = conn.cursor()
+
+    cur.execute(""" INSERT INTO registro (id_produto, qtde, operacao) VALUES (?,?, 'DEBITO') """, ( codigo, quantidade,))
+    conn.commit()
+    conn.close()
+    
+    atualizar_debito(codigo, quantidade)
+
+    return {"msg":"debito efetuado"}
+
+
+def atualizar_credito(codigo:int, quantidade:int):
+    conn = sqlite3.connect("/home/ijovem03/turma202402/database/estoque.db")
+    cur = conn.cursor()
+
+    cur.execute(""" UPDATE produto SET saldo = saldo + ? WHERE id = ? """, (quantidade,codigo))
+    conn.commit()
+    conn.close()
+    return 0
+
+def atualizar_debito(codigo:int, quantidade:int):
+    conn = sqlite3.connect("/home/ijovem03/turma202402/database/estoque.db")
+    cur = conn.cursor()
+
+    cur.execute(""" UPDATE produto SET saldo = saldo - ? WHERE id = ? """, (quantidade,codigo))
+    conn.commit()
+    conn.close()
+    return 0 
